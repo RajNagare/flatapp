dd447857128916c35a8220bbcfeaeeee04926896
Max Felker <max@bigroomstudios.com>
2015-02-02 14:36:45 -0500

    Setup clean up

--- app/bin/setup.sh
+++ app/bin/setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo "Welcome to FlatApp! We're going to install some dependecies to setup the project";
+
 echo "";
+echo "------------------------------------------------------";
+echo "Welcome to FlatApp! We're going to install some dependecies to setup the project";
+echo "------------------------------------------------------";
 
 echo "Installing Composer dependecies...";
 echo "";
 composer install
 
+echo ""
 echo "Installing Bower dependecies...";
 echo "";
 
 bower install
 
+unlink setup 
+
 echo "";
 echo "Project setup complete!"
+echo "";
